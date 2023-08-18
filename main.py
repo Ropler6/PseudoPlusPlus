2afad972c8adede40c507aded29c58a7f2abc053
= <=>
2023-08-18 18:39:19 +0300

    Add basic processing

--- main.py
+++ main.py
@@ -50,6 +50,7 @@ def add_character_at(character: str, string: str, position: int) -> str:
 
     return string[:position] + character + string[position:]
 
+
 def preprocess_larrow(line: str, pos: int):
     """
     Adds spaces around '<' (if required)
@@ -63,6 +64,7 @@ def preprocess_larrow(line: str, pos: int):
         pos += 1
 
     return line, pos
+
 
 def preprocess_rarrow(line: str, pos: int):
     """
@@ -97,8 +99,8 @@ def preprocess_minus(line: str, pos: int):
 
     return line, pos
 
-# TODO: handle += -= /= etc
 
+# TODO: handle += -= /= etc
 def preprocess_arithmetic_operator(line: str, pos: int):
     """
     Adds spaces around arithmetic operators: +/*%[]=()
@@ -116,14 +118,67 @@ def preprocess_arithmetic_operator(line: str, pos: int):
     return line, pos
 
 
+def process_user_input(line: str):
+    pass
+
+
+def process_user_output(line: str):
+    pass
+
+required_stops = 0 # the amount of stops required to close all loops/if statements
+required_loop_enders = 0 # the amount of "cat timp" required to close all repeta-loops
+def process_line(line: str):
+    segments = line.split(";")
+    if len(segments) > 1: #if there are more instructions, process them separately
+        result = ""
+        for segment in segments:
+            result += process_line(segment)
+
+        return result
+
+    tokens = segments[0].strip("\n").split()
+    result = ""
+    global required_stops, required_loop_enders
+    requires_semicolon = True
+
+    if len(tokens) == 0:
+        return ""
+
+    if tokens[0] == "citeste":
+        # return process_user_input(segments[0])
+        pass
+    elif tokens[0] == "scrie":
+        # return process_user_output(segments[0])
+        pass
+    elif tokens[0] in ["daca", "cat", ]:
+        required_stops += 1
+        requires_semicolon = False
+    elif tokens[0] == "repeta":
+        required_loop_enders += 1
+    elif tokens[0] == "stop":
+        required_stops -= 1
+        requires_semicolon = False
+
+    for token in tokens: #TODO: error-handling
+        if keywords.get(token) is not None:
+            result += keywords[token]
+        else:
+            result += token
+
+    if requires_semicolon:
+        result += ";"
+
+    return result + "\n"
+
 # Reset the rest file
 with open("./temp.txt", "w+") as g:
     g.write("")
 
+# Add spaces to the file and put the results in a temp file
 with open("./main.pc") as f:
     for line in f: # go line by line
         i = 0
-        while i < len(line): # processing the current line (adding spaces where needed)
+        while i < len(line): # processing the current line
                              # using a while-loop to properly have len(line) updated
             match line[i]:
                 case "<":
@@ -143,3 +198,12 @@ with open("./main.pc") as f:
         with open("./temp.txt", "a") as g:
             g.write(line)
 
+with open("./test.cpp", "w") as g:
+    g.write("")
+
+# TODO: move this in the for-loop above
+with open("./temp.txt") as f:
+    with open("./test.cpp", "a") as g:
+        for line in f:
+            processed_line = process_line(line)
+            g.write(processed_line)
