b50aa1a3f56500f49fbcddd51c097c7e9221d849
= <=>
2023-08-18 17:26:38 +0300

    Add comments

--- main.py
+++ main.py
@@ -43,9 +43,18 @@ keywords = {
 identifiers: list[Identifier] = []
 
 def add_character_at(character: str, string: str, position: int) -> str:
+    """
+    Adds the given `character` at the given `position` and returns the new string
+    """
+
+
     return string[:position] + character + string[position:]
 
 def preprocess_larrow(line: str, pos: int):
+    """
+    Adds spaces around '<' (if required)
+    Returns the modified line and the new position of the character 
+    """
     if line[pos + 1] != "-" and line[pos + 1] != " ":
         line = add_character_at(" ", line, pos + 1)
 
@@ -56,6 +65,12 @@ def preprocess_larrow(line: str, pos: int):
     return line, pos
 
 def preprocess_rarrow(line: str, pos: int):
+    """
+    Adds spaces around '>' (if required)
+    Returns the modified line and the new position of the character 
+    """
+
+
     if line[pos + 1] != "=" and line[pos + 1] != " ":
         line = add_character_at(" ", line, pos + 1)
 
@@ -65,7 +80,14 @@ def preprocess_rarrow(line: str, pos: int):
 
     return line, pos
 
+
 def preprocess_minus(line: str, pos: int):
+    """
+    Adds spaces around the minus (if required)
+    Returns the modified line and the new position of the character 
+    """
+
+
     if line[pos + 1] != " ":
         line = add_character_at(" ", line, pos + 1)
 
@@ -76,7 +98,14 @@ def preprocess_minus(line: str, pos: int):
     return line, pos
 
 # TODO: handle += -= /= etc
+
 def preprocess_arithmetic_operator(line: str, pos: int):
+    """
+    Adds spaces around arithmetic operators: +/*%[]=()
+    Returns the modified line and the new position of the character 
+    """
+
+
     if line[pos + 1] != " ":
         line = add_character_at(" ", line, pos + 1)
 
@@ -87,6 +116,7 @@ def preprocess_arithmetic_operator(line: str, pos: int):
     return line, pos
 
 
+# Reset the rest file
 with open("./test.cpp", "w") as g:
     g.write("")
 
@@ -109,6 +139,7 @@ with open("./main.pc") as f:
                     line, i = preprocess_arithmetic_operator(line, i)
             i += 1
 
+        # Write the processed line to the file
         with open("./test.cpp", "a") as g:
             g.write(line)
 
